cade5412972aced0e45b4363b3b8af85da66caf4
Dilmer Valecillos <dilmervst@gmail.com>
2019-03-12 01:13:28 -0600

    Created project, updated LWRP, added Shader Graph, and created a new shader

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotateSpeed;
+
+    void Update() { 
+        transform.Rotate(rotateSpeed * Time.deltaTime, Space.Self);    
+    }
+}
