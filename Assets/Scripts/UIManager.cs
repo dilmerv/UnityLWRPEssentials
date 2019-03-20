88fb4464a21b38436865d83cb5ed9ef2834a777d
Dilmer Valecillos <dilmervst@gmail.com>
2019-03-20 00:18:32 -0600

    Added new scene and material. Also UI that allows you to modify shader properties in real-time

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Slider timeSpeedSlider = null;
+
+    [SerializeField]
+    private Text timeSpeedSliderLabel = null;
+
+    [SerializeField]
+    private Slider noiseScaleSlider = null;
+
+    [SerializeField]
+    private Text noiseScaleSliderLabel = null;
+
+    [SerializeField]
+    private Material deformationMaterial = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(deformationMaterial != null){
+            // time speed 
+            deformationMaterial.SetFloat("_TimeSpeed", timeSpeedSlider.value);
+            timeSpeedSliderLabel.text = $"Time Speed: {timeSpeedSlider.value}";
+
+            // noise scale
+            deformationMaterial.SetFloat("_NoiseScale", noiseScaleSlider.value);
+            noiseScaleSliderLabel.text = $"Noise Scale: {noiseScaleSlider.value}";
+        }        
+    }
+}
